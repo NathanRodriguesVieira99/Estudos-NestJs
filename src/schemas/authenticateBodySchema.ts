0c51dfbea755d8c00768bb6b1bf13052b1611c7c
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-07-12 20:57:39 -0300

    feat(schemas): add authenticateBodySchema

---
+++ src/schemas/authenticateBodySchema.ts
@@ -0,0 +1,8 @@
+import { z } from 'zod';
+
+export const authenticateBodySchema = z.object({
+  email: z.email(),
+  password: z.string(),
+});
+
+export type AuthenticateBodySchema = z.infer<typeof authenticateBodySchema>;
