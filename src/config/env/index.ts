54523243d260d9cf579cc7199104e14c565e34e7
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-07-25 18:08:54 -0300

    feat: add CORS_ORIGIN and NODE_ENV to envSchema

--- src/config/env/index.ts
+++ src/config/env/index.ts
@@ -3,9 +3,10 @@ import z from 'zod';
 export const envSchema = z.object({
   DATABASE_URL: z.url().startsWith('postgresql://'),
   PORT: z.coerce.number().optional().default(3333),
+  NODE_ENV: z.string(),
   JWT_PRIVATE_KEY: z.string(),
   JWT_PUBLIC_KEY: z.string(),
-  CORS_ORIGIN: z.string(),
+  CORS_ORIGIN: z.url().startsWith('http://localhost:'),
 });
 
 export type Env = z.infer<typeof envSchema>;
