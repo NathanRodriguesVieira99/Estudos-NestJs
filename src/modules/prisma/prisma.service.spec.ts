fadbc6a561035b7b99d7c496c24f8cc21c71b3e3
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-07-27 19:20:36 -0300

    test: add unit tests for PrismaService

---
+++ src/modules/prisma/prisma.service.spec.ts
@@ -0,0 +1,29 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { PrismaService } from './prisma.service';
+import { ConfigModule } from '@nestjs/config';
+import { vi } from 'vitest';
+
+describe('PrismaService', () => {
+  let service: PrismaService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [ConfigModule.forRoot({ isGlobal: true })],
+      providers: [PrismaService],
+    }).compile();
+
+    service = module.get<PrismaService>(PrismaService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  it('should call $connect', async () => {
+    const spyConnect = vi.spyOn(service, '$connect');
+
+    await service.onModuleInit();
+
+    expect(spyConnect).toBeCalledTimes(1);
+  });
+});
