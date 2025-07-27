85c436bffb857bd465996075e8afaf04b08e63a2
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-07-27 19:19:34 -0300

    refactor: update environment file path and remove unused PrismaService provider

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,22 +5,23 @@ import { envSchema } from '@/config/env';
 import { AuthenticateController } from '@/controllers/authenticate.controller';
 import { CreateAccountController } from '@/controllers/create-account.controller';
 import { CreateQuestionController } from '@/controllers/create-question.controller';
-import { PrismaService } from '@/services/prisma/prisma.service';
+import { PrismaModule } from './modules/prisma/prisma.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: '.env.development',
+      envFilePath: '.env',
       validate: (env) => envSchema.parse(env),
       isGlobal: true,
     }),
     AuthModule,
+    PrismaModule,
   ],
   controllers: [
     CreateAccountController,
     AuthenticateController,
     CreateQuestionController,
   ],
-  providers: [PrismaService],
+  providers: [],
 })
 export class AppModule {}
