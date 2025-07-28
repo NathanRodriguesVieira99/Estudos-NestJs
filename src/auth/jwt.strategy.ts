67f2d231bf924392ef675a629bdf0543c483cbaa
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-07-27 21:52:46 -0300

    feat: implement JWT strategy

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,26 @@
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import type { Env } from '@/config/env';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import {
+    tokenPayloadSchema,
+    type UserPayload,
+} from '@/schemas/tokenPayloadSchema';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+    constructor(config: ConfigService<Env, true>) {
+        const publicKey = config.get('JWT_PUBLIC_KEY', { infer: true });
+
+        super({
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            secretOrKey: Buffer.from(publicKey, 'base64'),
+            algorithms: ['RS256'],
+        });
+    }
+
+    async validate(payload: UserPayload) {
+        return tokenPayloadSchema.parse(payload);
+    }
+}
