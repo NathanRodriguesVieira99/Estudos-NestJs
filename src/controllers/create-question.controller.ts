be2d0be9cdf624de66f118d7d00d2302d52e5c83
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-07-27 21:54:55 -0300

    feat: enhance CreateQuestionController to use JwtAuthGuard and CurrentUser decorator

--- src/controllers/create-question.controller.ts
+++ src/controllers/create-question.controller.ts
@@ -1,11 +1,17 @@
-import { Controller, Post } from '@nestjs/common';
+import { CurrentUser } from '@/auth/current-user-decorator';
+import { JwtAuthGuard } from '@/auth/jwt-auth.guard';
+import type { UserPayload } from '@/schemas/tokenPayloadSchema';
+import { Controller, Post, UseGuards } from '@nestjs/common';
 
 @Controller('/questions')
+@UseGuards(JwtAuthGuard)
 export class CreateQuestionController {
     constructor() {}
 
     @Post()
-    async execute() {
+    async execute(@CurrentUser() user: UserPayload) {
+        console.log(user);
+
         return 'ok';
     }
 }
