f07fb9085ed11495a04d0896733fb574e6cb4b27
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-07-27 21:50:43 -0300

    feat: create prisma seed file using faker-js and script to run this

---
+++ prisma/seed.ts
@@ -0,0 +1,40 @@
+import { PrismaClient, type User } from '../generated/prisma'; // !! FIX atualizar type User por uma entidade
+import { faker } from '@faker-js/faker';
+import { hashPassword } from '../src/utils/hash';
+
+const prisma = new PrismaClient();
+
+const seed = async () => {
+    await prisma.user.deleteMany();
+    await prisma.question.deleteMany();
+
+    const users: User[] = [];
+
+    for (let i = 0; i < 5; i++) {
+        const user = await prisma.user.create({
+            data: {
+                name: faker.person.fullName(),
+                email: faker.internet.email(),
+                password: await hashPassword('12345678'),
+            },
+        });
+
+        users.push(user);
+    }
+
+    for (let i = 0; i < 10; i++) {
+        const author = users[Math.floor(Math.random() * users.length)];
+        await prisma.question.create({
+            data: {
+                title: faker.lorem.sentence(),
+                slug: faker.string.uuid(),
+                content: faker.lorem.paragraph(),
+                authorId: author.id,
+            },
+        });
+    }
+};
+
+seed().then(() => {
+    console.log('Database seeded 🌱 ');
+});
