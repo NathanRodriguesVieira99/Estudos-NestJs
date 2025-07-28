2e8e3f3a415b2176b69aa6f0bcf93601c0886c30
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-07-28 12:25:26 -0300

    feat: improve test files config

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,35 +4,36 @@ import tsConfigPaths from 'vite-tsconfig-paths';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-  test: {
-    globals: true,
-    root: './',
-    coverage: {
-      reportsDirectory: './coverage',
-      reporter: ['text', 'html', 'lcov', 'cobertura'],
-      provider: 'v8',
-      include: ['src/**/*.{ts,js}'],
-      exclude: [
-        '**/*.test.{ts,tsx}',
-        '**/*.spec.{ts,tsx}',
-        '**/types/**',
-        '**/*.d.ts',
-        '**/mocks/**',
-        '**/*.test-utils.{ts,tsx}',
-        'src/main.ts',
-        '**/*.module.ts',
-      ],
+    test: {
+        globals: true,
+        root: './',
+        environment: 'node',
+        coverage: {
+            reportsDirectory: './coverage',
+            reporter: ['text', 'html', 'lcov', 'cobertura'],
+            provider: 'v8',
+            include: ['**/*.spec.ts'],
+            exclude: [
+                '**/*.test.{ts,js}',
+                '**/*.spec.{ts,js}',
+                '**/types/**',
+                '**/*.d.ts',
+                '**/mocks/**',
+                '**/*.test-utils.{ts,js}',
+                'src/main.ts',
+                '**/*.module.ts',
+            ],
+        },
     },
-  },
-  plugins: [
-    tsConfigPaths(),
-    swc.vite({
-      module: { type: 'es6' },
-    }),
-  ],
-  resolve: {
-    alias: {
-      src: resolve(__dirname, './src'),
+    plugins: [
+        tsConfigPaths(),
+        swc.vite({
+            module: { type: 'es6' },
+        }),
+    ],
+    resolve: {
+        alias: {
+            src: resolve(__dirname, './src'),
+        },
     },
-  },
 });
