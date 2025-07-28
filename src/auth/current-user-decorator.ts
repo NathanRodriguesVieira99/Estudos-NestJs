f3a0924bbb06409a1773a1b8fa0dada5151cb5ac
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-07-27 21:53:57 -0300

    feat: add CurrentUser decorator

---
+++ src/auth/current-user-decorator.ts
@@ -0,0 +1,10 @@
+import type { UserPayload } from '@/schemas/tokenPayloadSchema';
+import { createParamDecorator, type ExecutionContext } from '@nestjs/common';
+
+export const CurrentUser = createParamDecorator(
+    (_: never, context: ExecutionContext) => {
+        const request = context.switchToHttp().getRequest();
+
+        return request.user as UserPayload;
+    }
+);
