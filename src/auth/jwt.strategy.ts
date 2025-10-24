47f74108b3d366ff940205cbe972ed031e494907
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-10-23 21:52:48 -0300

    fix: update JwtStrategy to use userPayloadSchema for validation

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -3,24 +3,24 @@ import type { Env } from '@/config/env';
 import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import {
-	tokenPayloadSchema,
-	type UserPayload,
-} from '@/schemas/tokenPayloadSchema';
+  userPayloadSchema,
+  type UserPayload,
+} from '@/schemas/user-payload.schema';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-	constructor(config: ConfigService<Env, true>) {
-		const publicKey = config.get('JWT_PUBLIC_KEY', { infer: true });
+  constructor(config: ConfigService<Env, true>) {
+    const publicKey = config.get('JWT_PUBLIC_KEY', { infer: true });
 
-		super({
-			jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-			secretOrKey: Buffer.from(publicKey, 'base64'),
-			algorithms: ['RS256'],
-		});
-	}
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: Buffer.from(publicKey, 'base64'),
+      algorithms: ['RS256'],
+    });
+  }
 
-	async validate(payload: UserPayload) {
-		return tokenPayloadSchema.parse(payload);
-	}
+  async validate(payload: UserPayload) {
+    return userPayloadSchema.parse(payload);
+  }
 }
