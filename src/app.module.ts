7d87864f779296ec67aff04d3d6345aace502761
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-10-23 21:52:58 -0300

    fix: reorder imports and improve code organization in app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,27 +1,36 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { AuthModule } from '@/auth/auth.module';
 import { envSchema } from '@/config/env';
+
+/*
+ MODULES
+ */
+import { AuthModule } from '@/auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './modules/prisma/prisma.module';
+/*
+  CONTROLLERS
+ */
 import { AuthenticateController } from '@/controllers/authenticate.controller';
 import { CreateAccountController } from '@/controllers/create-account.controller';
 import { CreateQuestionController } from '@/controllers/create-question.controller';
-import { PrismaModule } from './modules/prisma/prisma.module';
+import { FetchRecentQuestionsController } from './controllers/fetch-recent-questions.controller';
 
 @Module({
-	imports: [
-		ConfigModule.forRoot({
-			envFilePath: '.env',
-			validate: (env) => envSchema.parse(env),
-			isGlobal: true,
-		}),
-		AuthModule,
-		PrismaModule,
-	],
-	controllers: [
-		CreateAccountController,
-		AuthenticateController,
-		CreateQuestionController,
-	],
-	providers: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true,
+    }),
+    AuthModule,
+    PrismaModule,
+  ],
+  controllers: [
+    CreateAccountController,
+    AuthenticateController,
+    CreateQuestionController,
+    FetchRecentQuestionsController,
+  ],
+  providers: [],
 })
 export class AppModule {}
